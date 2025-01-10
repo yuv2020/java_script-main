1daa5cc0dab64d4e4258eec9370008decd0ff07b
yuv2020 <yuvraj252122@gmail.com>
2025-01-11 00:30:03 +0530

    add in files

---
+++ basics/conversionoperations.js
@@ -0,0 +1,35 @@
+let score = "Hi";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+//"33" >= 33
+//"33abc" <= NaN
+//true = 1 & false = 0
+
+let isloggedin = "trw";
+let booleanIsLoggedin = Boolean(isloggedin);
+
+console.log("The boolean value is: " + booleanIsLoggedin);
+
+
+// 0 >= false; 1 >= true; 
+//  "" >= false;
+// "trw" >= true;
+
+let sum = 33;
+let s = 21;
+
+let str = String(sum);
+let str2 = String(s);
+
+console.log("number to string conversion " + str + str2);
+console.log(typeof str2);
+console.log(typeof str);
+
