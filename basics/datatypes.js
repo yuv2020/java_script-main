b57061e126088cde5461ff1e20f1030a02f80ef7
yuv2020 <yuvraj252122@gmail.com>
2025-01-11 00:07:24 +0530

    adding

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version.
+
+// alert(3+3);     // we're using node js not browser
+
+console.log(3+
+    3);     //Code readibility should be high 
+    
+console.log("priya"); // correct syntax
+
+let name = "priya";
+let age = 12;
+let islogged = false;
+let state = null;
+
+//All primitives Data Types
+//number >= 2 to power 53
+//bigint
+//string
+//boolean >= true / false
+//null  (It is a data type but also a stand alone value)
+//undefined
+//symbol >= uniqueness
+
+
+//Object
+
+console.table([typeof age, typeof name, typeof state, typeof islogged]);
--- basics/variables.js
+++ basics/variables.js
@@ -3,3 +3,21 @@ let accountemail = "yuv@gmail.com";
 var accoutpassword = "32tyvrd"
 
 accountcity = "jodhpur";
+
+// accountId = 135      // not allowed
+
+accountemail = "raj@gmail.com";
+accountpassword = "231874";
+accountcity = "jaipur";
+
+let accountstate;
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountemail, accountpassword, accountcity, accountstate]);
+
