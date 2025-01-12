951790b47ddd59451d7c0e34a634ec69d89454c7
yuv2020 <yuvraj252122@gmail.com>
2025-01-12 17:49:06 +0530

    all

---
+++ basics/comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2!=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2<1);
+
+// console.log(undefined>7);
+
+/* Comparison and equality check both are different things. */
+
+/* === is known as strict check */
+console.log(2 == "2"); // true
+console.log(2 === "2"); // false
+
+console.log(true == 1); // true
+console.log(true === 1); // false
+
--- basics/conversionoperations.js
+++ basics/conversionoperations.js
@@ -1,35 +1,71 @@
-let score = "Hi";
+// let score = "Hi";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
-
-
-//"33" >= 33
-//"33abc" <= NaN
-//true = 1 & false = 0
-
-let isloggedin = "trw";
-let booleanIsLoggedin = Boolean(isloggedin);
-
-console.log("The boolean value is: " + booleanIsLoggedin);
+// console.log(valueInNumber);
 
 
-// 0 >= false; 1 >= true; 
-//  "" >= false;
-// "trw" >= true;
+                                        //"33" >= 33
+                                        //"33abc" <= NaN
+                                        //true = 1 & false = 0
 
-let sum = 33;
-let s = 21;
+// let isloggedin = "trw";
+// let booleanIsLoggedin = Boolean(isloggedin);
 
-let str = String(sum);
-let str2 = String(s);
+// console.log("The boolean value is: " + booleanIsLoggedin);
 
-console.log("number to string conversion " + str + str2);
-console.log(typeof str2);
-console.log(typeof str);
+
+                                        // 0 >= false; 1 >= true; 
+                                        //  "" >= false;
+                                        // "trw" >= true;
+
+// let sum = 33;
+// let s = 21;
+
+// let str = String(sum);
+// let str2 = String(s);
+
+// console.log("number to string conversion " + str + str2);
+// console.log(typeof str2);
+// console.log(typeof str);
+
+
+
+// ********************************* OPERATIONS *********************************
+let value = 3;
+let neg = -value;
+console.log(neg);
+
+let str1 = "gupta";
+let str2 = "213";
+
+let str = str1 + str2;
+console.log(str);
+
+console.log("1");
+console.log("1" + 2);
+console.log(2 + "1");
+console.log("1" + 3 + 2);
+console.log(2 + 1 + "2");
+
+// console.log((3+4) * 23 + 76 % 1);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let n1, n2, n3, n4;
+n1 = n2 = n3 = 2 + 2;
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+
+
+
+
 
