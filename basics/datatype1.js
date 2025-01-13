3dea7ccfd21fcf7c910aa9e2646cca163593ed0b
yuv2020 <yuvraj252122@gmail.com>
2025-01-13 22:11:47 +0530

    commit succesful

--- basics/datatype1.js
+++ basics/datatype1.js
@@ -2,22 +2,22 @@
 
 // 7 categories of primitive data types : String, Number, Boolean, null, udefined, Symbol, BigInt
 
-const score = 100;
-const scorevalue = 100.2;
+// const score = 100;
+// const scorevalue = 100.2;
 
-const name = "Yuvraj";
-const isloggedin = false;
-let usernames;
+// const name = "Yuvraj";
+// const isloggedin = false;
+// let usernames;
 
 // const id = Symbol('12');
-const anotherid = Symbol('12');
+// const anotherid = Symbol('12');
 
-console.table([typeof anotherid, anotherid]); 
+// console.table([typeof anotherid, anotherid]); 
 
-const bignumber = 2376293809945n;
+// const bignumber = 2376293809945n;
 
-console.log(typeof bignumber);  // BigInt   
-console.log(bignumber)
+// console.log(typeof bignumber);  // BigInt   
+// console.log(bignumber)
 
 /* Reference(Non-primitive data types)
 1. Array
@@ -25,14 +25,38 @@ console.log(bignumber)
 3. Functions
 */
 
-const heroes = ['Shakespeare', 'ironman', 'hero', 'baalveer']
-let obj = {
-    name: 'Rohit',
-    age: 30,
+// const heroes = ['Shakespeare', 'ironman', 'hero', 'baalveer']
+// let obj = {
+//     name: 'Rohit',
+//     age: 30,
+// }
+
+// const mufunct = function (){
+//     console.log('I am a function');
+// }
+
+// mufunct();
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Two types of memory (Stack i.e., used in the by the primitive And Heap i.e., used by the non-primitive)
+let myname = "hireme";
+let anothername = myname;
+anothername = 'any'
+
+console.log(myname); // hireme
+console.log(anothername);
+
+let userone = {
+    email : "userone@gmail.com",
+    upi : "upi@sbi",
 }
 
-const mufunct = function (){
-    console.log('I am a function');
-}
+let usertwo = userone;
 
-mufunct();
+usertwo.email = 'yuv@gmail.com';
+
+console.table([userone, usertwo, usertwo.email, userone.email])
+
---
+++ basics/strings.js
@@ -0,0 +1,34 @@
+const str = "yuv2020";
+const repo = 30;
+// console.log(str + repo + " author ");
+
+//String Interpolation 
+console.log(`Hello my name is ${str} and my repos are 30`);        //use the backtics(``) method
+
+const str1 = new String("Publisher")
+console.log(str1);
+console.log(str1[5]);
+console.log(str1.__proto__);
+console.log(str1.toUpperCase());
+console.log(str1.charAt(5));
+console.log(str1.indexOf('e'));
+
+const newstr = str1.substring(0,5);
+
+console.log(newstr);
+
+const str2 = str1.slice(-8,5);
+console.log("reversed string is : " + str2);
+
+const new_string = "    reversed    ";
+console.log(new_string);
+console.log(new_string.trim());
+
+const url = "https://youtube.com"
+console.log(url.replace( 'tube' - 'you'));
+console.log(url.includes('sundar'));
+
+
+console.log(url.startsWith("https://"));
+
+
